9c066314b3f373b233ad4ae4b43d27a0022ed8be
Yan Santana <yansbarreiro@gmail.com>
2020-06-10 17:07:22 -0300

    :sparkles: Products

--- src/controllers/ItemsController.ts
+++ src/controllers/ItemsController.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from 'express';
-import Knex from 'knex';
+import knex from '../database/connection';
 
 export default class ItemsController {
   async index(request: Request, response: Response) {
-    const items = await Knex('items').select('*');
+    const items = await knex('items').select('*');
 
     const serializedItems = items.map(item => {
       return {
@@ -14,5 +14,41 @@ export default class ItemsController {
     });
 
     return response.json(serializedItems);
+  }
+
+  async show(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+
+    const products = await knex('items').where('id', id).first();
+
+    if (!products) {
+      return response.status(400).json({ message: 'Product not found!' });
+    }
+
+    return response.json({ products });
+  }
+
+  async create(request: Request, response: Response): Promise<Response> {
+    const { price, title } = request.body;
+
+    const product = { price, title };
+
+    await knex('items').insert(product);
+
+    return response.json(product);
+  }
+
+  async update(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+    const { price, title } = request.body;
+
+    await knex('items').where('id', id).update({ price, title });
+
+    const updatedOrder = await knex('items')
+      .where('id', id)
+      .select('*')
+      .first();
+
+    return response.json({ updatedOrder });
   }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,19 @@
 import express from 'express';
 import OrdersCotroller from './controllers/OrdersController';
+import ItemsController from './controllers/ItemsController';
 
 const routes = express.Router();
 const ordersController = new OrdersCotroller();
+const itemsController = new ItemsController();
 
 routes.post('/orders', ordersController.create);
 routes.get('/orders/:id', ordersController.show);
 routes.get('/orders', ordersController.index);
 routes.put('/orders', ordersController.update);
+
+routes.get('/products/:id', itemsController.show);
+routes.get('/products', itemsController.index);
+routes.post('/products', itemsController.create);
+routes.put('/products/:id', itemsController.update);
 
 export default routes;
